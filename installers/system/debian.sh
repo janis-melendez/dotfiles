66e49423c13dc438a4bbcbeaf2d9f573d2840dd4
janis-h <janis_h.aguilar@yahoo.com>
2026-09-08 11:53:59 -0700

    installers: remove setup_papirus_repository (debian)

--- installers/system/debian.sh
+++ installers/system/debian.sh
@@ -145,40 +145,12 @@ setup_helium_browser_repository() {
     echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/helium.gpg] https://pkg.helium.computer/deb stable main" | sudo tee /etc/apt/sources.list.d/helium.list
 }
 
-setup_papirus_repository() {
-    local pkgs=(
-        "qt6-style-kvantum"
-        "arc-kde"
-    )
-
-    local repo="papirus/papirus"
-    local ppa="ppa:papirus/papirus"
-
-    local configure_repo=false
-
-    for pkg in "${pkgs[@]}"; do
-        if is_in_packages_list "$pkg" "$@" ; then
-            configure_repo=true
-            break
-        fi
-    done
-
-    if ! "$configure_repo" || is_repository_configured "$repo"; then
-        return 0
-    fi
-
-    info "Configuring Papirus repository"
-
-    run_cmd sudo add-apt-repository -y "$ppa" || return 1
-}
-
 setup_package_repositories() {
     info "Configuring package repositories"
 
     setup_1password_repository "$@"
     setup_chrome_repository "$@"
     setup_helium_browser_repository "$@"
-    setup_papirus_repository "$@"
 }
 
 # --- Public entrypoint ---
